076f46c7614c64d53bf45b1938c820f98f6fbcae
ayorri <alliekim24@marlborough.org>
2022-09-23 08:47:56 -0700

    Complete FIbonacci

---
+++ Unit1/Fibonacci.java
@@ -0,0 +1,15 @@
+public class Fibonacci
+{
+    public static void main(String[] args)
+    {
+        int a = 0, b = 1, c;
+
+        for (int i = 0; i<50; i++)
+        {
+           c = a+b;
+           System.out.println(c);
+           a = b;
+           b = c;
+        }
+    }
+}
