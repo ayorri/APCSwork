6ea750f8a2cd2e19ed6ee9349b78946f01c31d75
ayorri <alliekim24@marlborough.org>
2022-12-14 09:40:52 -0800

    completed

--- Unit5/TestMagicEightBall.java
+++ Unit5/TestMagicEightBall.java
@@ -5,13 +5,16 @@ public class TestMagicEightBall
     MagicEightBall ball = new MagicEightBall();
     MagicEightBall ball2 = new MagicEightBall();
     MagicEightBall ball3 = new MagicEightBall();
+    MagicEightBall ball4 = new MagicEightBall();
 
     ball.ask();
     ball2.ask();
     ball3.ask();
+    ball4.ask();
 
     System.out.println("Will I become rich? " + ball.ask());
     System.out.println("Will I ace my test tomorrow? " + ball2.ask());
     System.out.println("Am I going to get Juniors this weekend? " + ball3.ask());
+    System.out.println("Am I gonna go to a good college " + ball4.ask());
    }
 }
